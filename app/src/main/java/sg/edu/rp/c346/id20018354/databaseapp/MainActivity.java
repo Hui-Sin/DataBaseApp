dd2e4de058dd32db76553b8e1087a06cdda3955d
Hui-Sin <20018354@myrp.edu.sg>
2021-07-09 12:11:50 +0800

    database get task

--- app/src/main/java/sg/edu/rp/c346/id20018354/databaseapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20018354/databaseapp/MainActivity.java
@@ -3,12 +3,17 @@ package sg.edu.rp.c346.id20018354.databaseapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnInsert;
+    Button btnInsert,btnGetTasks;
+    TextView tvResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnInsert = findViewById(R.id.btnInsert);
+        btnGetTasks=findViewById(R.id.btnGetTasks);
+        tvResults=findViewById(R.id.tvResults);
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,5 +31,25 @@ public class MainActivity extends AppCompatActivity {
                 db.insertTask("Submit RJ", "25 Apr 2021");
             }
         });
+        btnGetTasks.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                ArrayList<String> data = db.getTaskContent();
+                db.close();
+
+                String txt = "";
+                for (int i = 0; i < data.size(); i++) {
+                    Log.d("Database Content", i +". "+data.get(i));
+                    txt += i + ". " + data.get(i) + "\n";
+                }
+                tvResults.setText(txt);
+            }
+        });
+
     }
 }
