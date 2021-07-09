6a45841a39641bf7a0561c240015e835a47675d7
Hui-Sin <20018354@myrp.edu.sg>
2021-07-09 11:48:08 +0800

    database

--- app/src/main/java/sg/edu/rp/c346/id20018354/databaseapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20018354/databaseapp/MainActivity.java
@@ -3,12 +3,26 @@ package sg.edu.rp.c346.id20018354.databaseapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInsert = findViewById(R.id.btnInsert);
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DBHelper db = new DBHelper(MainActivity.this);
+                db.insertTask("Submit RJ", "25 Apr 2021");
+            }
+        });
     }
 }
